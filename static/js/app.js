b8ac002e0bbcee24aa73b5aad3bc49b32d87993c
Andy Herron <coachfozzie@gmail.com>
2022-01-27 22:51:03 -0500

    working on module

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+for (var i = 0; i < 5; i++) {
+    console.log("I am " + i);
+ }
+
+ 
